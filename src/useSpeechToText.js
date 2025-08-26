d2fc20527cdbde4e3e051bf9084306884ebf1bf1
JiHyoung You <fbwlgud51@gmail.com>
2025-08-26 21:53:00 +0900

    fix: 되나??

--- src/useSpeechToText.js
+++ src/useSpeechToText.js
@@ -7,14 +7,18 @@ const useSpeechToText = () => {
     useSpeechRecognition();
 
   const toggleListening = () => {
+    console.log("버튼 눌림");
+
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
       alert("이 브라우저는 음성 인식을 지원하지 않습니다.");
       return;
     }
 
     if (listening) {
+      console.log("음성 인식 중지");
       SpeechRecognition.stopListening();
     } else {
+      console.log("음성 인식 시작");
       resetTranscript();
       SpeechRecognition.startListening({
         language: "ko-KR",
